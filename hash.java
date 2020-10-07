533bc99838c20e8774037dc83686622c71913f6a
Rishabh Sharma <r1998glb@gmail.com>
2020-10-07 09:21:52 +0530

    initial commit

---
+++ hash.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class A
+{
+public static void main(String...args){
+Scanner ob=new Scanner(System.in);
+int n=ob.nextInt();
+int i,j;
+for(i=1;i<=n;i++){
+	if(i==1){
+		System.out.print("*");
+		}
+	else if(i>1&&i<n){
+		System.out.print("* ");
+		for(j=1;j<=i-1;j++){
+			System.out.print("  ");
+				}
+		System.out.print("*");
+		}
+	else{
+		for(j=1;j<=n;j++){
+			System.out.print("* ");
+			}
+	}
+	System.out.println();
+}
+}
+}
